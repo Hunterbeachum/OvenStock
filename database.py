f51c415c16be3a155770f5bf2077429c0936c5df
deppong <isaiahjdeppong@gmail.com>
2023-02-02 17:45:16 -0500

    reorganized some file structure and code

--- database.py
+++ database.py
@@ -3,12 +3,24 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect('database.db')
 
-    with open('res/schema.sql') as file:
+    with open('schema.sql') as file:
         conn.executescript(file.read())
 
     conn.commit()
     conn.close()
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    return conn
+    return sqlite3.connect('database.db')
+    
+
+# executes a sql statement and returns the output
+def exec_sql(statement):
+    conn = get_db_connection()
+
+    cur = conn.cursor()
+    result = cur.execute(statement).fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import database
 
 database.init_db()
 
-conn = database.get_db_connection()
-cur = conn.cursor()
 
-cur.execute("INSERT INTO product (vendor, quantity) VALUES ('Panera Bread', 5);")
-print(cur.execute("SELECT * FROM product;").fetchall())
-
-conn.commit()
-conn.close()
+print(database.exec_sql("INSERT INTO product (vendor, quantity) VALUES ('Panera Bread', 5);"))
+print(database.exec_sql("SELECT * FROM product;"))
+print(database.exec_sql("pragma table_info('product')"))
