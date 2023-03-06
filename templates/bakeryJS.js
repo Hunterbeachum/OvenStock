58e2fc7496cb3b524953df33f36450b8901d8ddc
cbagosta <cbagosta@my.waketech.edu>
2023-03-06 14:42:29 -0500

    Adding a menu overlay for inventory that is out of stock to be displayed.

---
+++ templates/bakeryJS.js
@@ -0,0 +1,7 @@
+function openOverlay() {
+    document.getElementById("message_overlay").style.width = "fit-content";
+}
+
+function closeOverlay() {
+    document.getElementById("message_overlay").style.width = "0";
+}
