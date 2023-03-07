e3ab1741fed33c50c05b9310f35df39e5a3c8284
cbagosta <cbagosta@my.waketech.edu>
2023-03-06 20:01:31 -0500

    Adding a function to create a list of out-of-stock items, and then creating an overlay that can display this list.

--- static/js/bakeryJS.js
+++ static/js/bakeryJS.js
@@ -1,17 +1,19 @@
 function openOverlay() {
     document.getElementById("message_overlay").style.width = "fit-content";
+    document.getElementById("message_overlay").style.opacity = "0%";
 }
 
 function closeOverlay() {
     document.getElementById("message_overlay").style.width = "0";
+    document.getElementById("message_overlay").style.opacity = "100%";
 }
 
-function onload_outOfStock() {
-    $.ajax({
-        url: "/main.py",
-        context: document.body
-    }).done(function( o ) {
-        // check if stock == 0
-
-    })
-}
+//function onload_outOfStock() {
+//     $.ajax({
+//        url: "/main.py",
+//        context: document.body
+//     }).done(function( o ) {
+// check if stock == 0
+//
+//     })
+// }
