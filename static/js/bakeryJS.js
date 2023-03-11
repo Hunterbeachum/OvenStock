5b95eefaf3c7375f1e57ca3167671a3f92e903f2
cbagosta <cbagosta@my.waketech.edu>
2023-03-11 15:15:18 -0500

    Adding use to the Out of Stock button on the inventory page, as well as adding some css and javascript code to make the button work. Removing the button from the layout.html page.

--- static/js/bakeryJS.js
+++ static/js/bakeryJS.js
@@ -2,20 +2,12 @@ function openOverlay() {
     document.getElementById("message_overlay").style.width = "fit-content";
     document.getElementById("message_overlay").style.opacity = "100%";
     document.getElementById("hideOver").style.opacity = "100%";
+    document.getElementById("message_overlay").style.height = "70px";
 }
 
 function closeOverlay() {
-    document.getElementById("message_overlay").style.width = "0";
+    document.getElementById("message_overlay").style.width = "0px";
     document.getElementById("message_overlay").style.opacity = "0%";
     document.getElementById("hideOver").style.opacity = "0%";
+    document.getElementById("message_overlay").style.height = "0px";
 }
-
-//function onload_outOfStock() {
-//     $.ajax({
-//        url: "/main.py",
-//        context: document.body
-//     }).done(function( o ) {
-// check if stock == 0
-//
-//     })
-// }
