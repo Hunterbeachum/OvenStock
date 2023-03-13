b54e0e924b1ac3bb5b5c78322fc7fa9c1cd0f487
cbagosta <cbagosta@my.waketech.edu>
2023-03-13 14:54:59 -0400

    Adding color to the table, changing the location that the 'show Out-of-order' button is displayed in, and adding pictures to create a more comfortable user experience.

--- static/js/bakeryJS.js
+++ static/js/bakeryJS.js
@@ -2,12 +2,12 @@ function openOverlay() {
     document.getElementById("message_overlay").style.width = "fit-content";
     document.getElementById("message_overlay").style.opacity = "100%";
     document.getElementById("hideOver").style.opacity = "100%";
-    document.getElementById("message_overlay").style.height = "70px";
+    document.getElementById("message_overlay").style.height = "40px";
 }
 
 function closeOverlay() {
     document.getElementById("message_overlay").style.width = "0px";
     document.getElementById("message_overlay").style.opacity = "0%";
     document.getElementById("hideOver").style.opacity = "0%";
-    document.getElementById("message_overlay").style.height = "0px";
+    document.getElementById("message_overlay").style.height = "20px";
 }
